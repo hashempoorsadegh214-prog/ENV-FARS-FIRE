54b41fb9a31c13499e09ccce21eada920491ec91
hashempoorsadegh214-prog <hashempoor.sadegh214@gmail.com>
2026-08-16 02:00:07 +0330

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+
+    boundary:
+        "fars.geojson",
+
+    protected:
+        "protected_areas.geojson",
+
+    hunting:
+        "hunting_banned.geojson",
+
+    archiveIndex:
+        "archive/index.json",
+
+    fallbackCsv:
+        "fires.csv",
+
+    updateInfo:
+        "update-info.json"
+
+};
